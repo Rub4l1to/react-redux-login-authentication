3a19d3f4134154cabd860fdb916897d95edf67b1
Adrian <adrian.perez@hipoo.com>
2022-06-08 12:38:16 +0200

    Some changes

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { PrivateOutlet } from 'layouts/PrivateLayout';
 
 //* Pages
-import { HomePage, LoginPage, AboutPage } from 'pages';
+import { HomePage, LoginPage } from 'pages';
 
 export const Navigation = () => {
   return (
@@ -16,12 +16,11 @@ export const Navigation = () => {
         {/* Private routes */}
         <Route path="/" element={<PrivateOutlet />}>
           <Route index element={<HomePage />} />
-          <Route path="about" element={<AboutPage />} />
         </Route>
         {/* Public routes */}
         <Route path="/login" element={<LoginPage />} />
 
-        <Route path="/*" element={<Navigate to="/login" replace />} />
+        <Route path="/*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
--- src/services/localStorage.ts
+++ src/services/localStorage.ts
@@ -1,12 +1,8 @@
 //* Interfaces
 import { LoginResponse } from '../interfaces/auth';
 
-export const setTokens = ({ token }: LoginResponse) => {
-  localStorage.setItem('token', JSON.stringify(token));
-};
+export const setTokens = ({ token }: LoginResponse) => localStorage.setItem('token', JSON.stringify(token));
 
-export const removeTokens = () => {
-  localStorage.removeItem('token');
-};
+export const removeTokens = () => localStorage.removeItem('token');
 
 export const getAccessToken = () => localStorage.getItem('token')?.slice(1, -1) || false;
