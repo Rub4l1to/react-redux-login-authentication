338b983db1fff49b83a9b7f6822406bc645fb289
Adrian <adrian.perez@hipoo.com>
2022-06-08 08:49:36 +0200

    Initial folder strucuture

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+//* Pages
+import { HomePage } from './pages';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <HomePage />
+  </React.StrictMode>,
+);
